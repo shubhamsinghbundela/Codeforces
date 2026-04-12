ce80c0c5cbf6cf3d82354030dd18b86487810ea1
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-04-13 00:00:16 +0530

    solved search queries, search queries || and first and last occurance

---
+++ Binary Search/First and Last Occurence/1st solution.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+  
+    
+    for (int i = 0; i < q; i++) {
+        int target;
+        cin>>target;
+        int l1=0;
+        int r1=n-1;
+        int ans = -1;
+        while(l1<=r1){
+            int mid = (l1+r1)/2;
+            if(arr[mid]==target){
+                ans= mid+1;
+                r1=mid-1;
+            }else if(arr[mid]<target){
+                l1=mid+1;
+            }else{
+                r1=mid-1;
+            }
+        }
+        if(ans!=-1){
+           cout<<ans<<" ";   
+        }else{
+            cout<<ans<<endl;   
+            continue;
+        }
+        
+        int l2=0;
+        int r2=n-1;
+        int ans1 = -1;
+        while(l2<=r2){
+            int mid = (l2+r2)/2;
+            if(arr[mid]==target){
+                ans1= mid+1;
+                l2=mid+1;
+            }else if(arr[mid]<target){
+                l2=mid+1;
+            }else{
+                r2=mid-1;
+            }
+        }
+        
+        cout<<ans1<<endl;
+    }
+}
---
+++ Binary Search/Search Queries/1st solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int long long
+ 
+signed main() {
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+  
+    
+    for (int i = 0; i < q; i++) {
+        int target;
+        cin>>target;
+        int l=0;
+        int r=n-1;
+        bool found = false;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid]==target){
+                cout<<mid+1<<endl;
+                found = true;
+                break;
+            }else if(arr[mid]<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        if (!found) cout << -1 << endl;
+    }
+}
