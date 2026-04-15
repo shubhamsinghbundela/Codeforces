69ee1a2e6c9f1a453304f0de55db2d2b6f27b62c
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-04-15 23:10:24 +0530

    using prefix sum for count vowel

--- Sliding Window And Two Pointer/Count Vowels/1st solution.cpp
+++ Sliding Window And Two Pointer/Count Vowels/1st solution.cpp
@@ -3,7 +3,53 @@
 using namespace std;
  
 #define int long long
+
+//Tried using prefix sum
+#include <bits/stdc++.h>
  
+using namespace std;
+ 
+#define int long long
+ 
+signed main() {
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    string str;
+    cin>>str;
+    int arr[n];
+    for (int i = 0; i < str.size(); i++) {
+        if( 
+            str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' ||
+          str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' 
+        ){
+            arr[i]=1;
+        }else{
+            arr[i]=0;
+        }
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum+=arr[i];
+        arr[i]=sum;
+    }
+    
+    int l=0;
+    int r=k-1;
+    while(r<n){
+        if(l==0){
+            cout<<arr[r]<<" ";
+        }else{
+            cout<<arr[r]-arr[l-1]<<" ";
+        }
+        l++;
+        r++;
+    }
+}
+ 
+// Tried using sliding window
 signed main() {
     int n;
     cin >> n;
