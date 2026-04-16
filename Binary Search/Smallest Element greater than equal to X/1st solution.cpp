683a4b25ab3a43553e8d085c3de0cff5943a2b72
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-04-17 02:07:55 +0530

    smallest element >=x and largest element <=x

---
+++ Binary Search/Smallest Element greater than equal to X/1st solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int long long
+ 
+signed main() {
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr + n);
+  
+    
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin>>x;
+        int l=0;
+        int r=n-1;
+        int ans = -1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid]>=x){
+                r=mid-1;
+                ans=arr[mid];
+            }else{
+                l=mid+1;
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
